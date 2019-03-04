ebcaf9a6026c0b65417ebb72de3bf58a078153b9
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-03-04 21:35:46 +0900

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject clj-rest-api "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure REST API example"
+  :url "https://github.com/lagenorhynque/clj-rest-api"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [duct/core "0.7.0"]
@@ -23,5 +23,5 @@
    :project/dev  {:source-paths   ["dev/src"]
                   :resource-paths ["dev/resources"]
                   :dependencies   [[integrant/repl "0.3.1"]
-                                   [eftest "0.5.4"]
+                                   [eftest "0.5.7"]
                                    [kerodon "0.9.0"]]}})
