862f8a57b204e36ecad63e1d670a6bb58ef37f76
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-11-04 22:21:09 +0900

    Introduce duct.module.cambium

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :min-lein-version "2.0.0"
   :dependencies [[camel-snake-kebab "0.4.0"]
                  [clojure.java-time "0.3.2"]
+                 [duct.module.cambium "0.1.0"]
                  [duct.module.pedestal "2.0.2"]
                  [duct/core "0.7.0"]
-                 [duct/module.logging "0.4.0"]
                  [duct/module.sql "0.5.0"]
                  [funcool/struct "1.4.0" :exclusions [org.clojure/clojurescript]]
                  [honeysql "0.9.8"]
